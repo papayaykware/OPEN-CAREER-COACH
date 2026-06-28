f2e5951e188d8f79427fbc7aadaaa2214b7fe0eb
Papayaykware <javierciborrogranados@protonmail.com>
2026-06-28 13:55:21 +0100

    Delete db directory

--- db/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Módulo de persistencia SQLite — OPEN-CAREER-COACH v2.0.0"""
-from .database import Database
-from .analysis_repository import AnalysisRepository
-
-__all__ = ["Database", "AnalysisRepository"]
